329afbce3c9a8b7bdf70ddb499371d05e9cbd0c1
Simon Gao <simongao398@gmail.com>
2026-01-14 01:19:01 -0500

    TODO: Make all routes on randomChatRoutes

--- yappr/backend/routes/randomChatRoutes.ts
+++ yappr/backend/routes/randomChatRoutes.ts
@@ -4,4 +4,16 @@ import db from '../database.js';
 import mysql from 'mysql2/promise';
 
 const router = express.Router();
+// TODO :
+
+/*
+- JOIN QUEUE
+- MAKE CHAT
+- DISPLAY CHAT (users + messages) /IF IN QUEUE
+- DISPLAY # PPL IN QUEUE
+- SEND MESSAGE (random_chat = TRUE)
+- LEAVE CHAT (make available = TRUE)
+
+*/
+
 export default router;
